2a200bf30d6ea891e0fb9e39e3bab1cf3a1de7ed
Dmitry Stoletov <irvis@imega.ru>
2022-11-14 12:56:13 +0300

    add draft
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- main.go
+++ main.go
@@ -13,6 +13,36 @@ import (
 	"github.com/rs/zerolog"
 )
 
+//+start_at:<=6 +end_at:>=10
+// TAKE BOOKING
+
+// select * from timeslot where start_at<=10 and end_at>=11
+// update timeslot set end_at=10 where id=?
+// insert timeslot (lot_id,start_at,end_at) values (?, 11, 65536)
+
+// RETURN BOOKING
+
+// Return booking with center-append
+// select * from timeslot where end_at=20 or start_at=21
+// if return empty you should insert record with start_at=20 and end_at=21
+
+// if return booking timeslot left-append
+// select * from timeslot where end_at=19 or start_at=20
+// +----+--------+----------+--------+
+// | id | lot_id | start_at | end_at |
+// +----+--------+----------+--------+
+// |  2 |      1 |       15 |     19 |
+// +----+--------+----------+--------+
+// update timeslot set end_at=20 where id=2
+
+// return booking with right-append
+// select * from timeslot where end_at=21 or start_at=22
+// +----+--------+----------+--------+
+// | id | lot_id | start_at | end_at |
+// +----+--------+----------+--------+
+// |  5 |      1 |       22 |  65535 |
+// +----+--------+----------+--------
+
 const (
 	shutdownTimeout = 15 * time.Second
 
